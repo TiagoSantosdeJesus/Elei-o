1b02c8f37436cab6c89eed840c7bda007148f681
TiagoSantosdeJesus <tiagosantos2561@hotmail.com>
2022-12-26 14:57:26 -0300

    Atualização

---
+++ Módulo_Web_Services_Sever/CRUD/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const porta = 8080;
+
+// array de livros que vamos utilizar como se fosse o banco de dados
+let livros = [];
+
+app.use(cors());
+
+//configurando o body parse para que o body das requisições seja interpretado
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+
+app.get('/books', (req, res) => {
+    res.send(livros)
+});
+
+app.post('/books', (req,res) => {
+    const {livro} = req.body;
+
+    // printando o livro que foi inserido
+    console.log(livro)
+
+    // add no array de livros
+    livros.push(livro)
+
+    res.send("Livro inserido com sucesso");
+});
+
+app.put('/books/:index', (req,res) => {
+    const {index} = req.params;
+    const {livro} = req.body;
+
+    livros[index] = livro;
+    res.send("Atualizado com sucesso");
+});
+
+app.delete('/books/:index', (req, res) => {
+    const {index} = req.params;
+    livros.splice(index, 1);
+    res.send("Excluído com sucesso");
+});
+
+
+app.listen(porta, () =>{
+    console.log("Servidor escutando na porta 8080")
+});
